593d32ac25c537a4e6e36f33ae7157e7f8fb3530
Jaison E Mathew <jaisonemathew14@gmail.com>
2024-07-14 01:08:28 +0530

    d

--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -17,7 +17,7 @@ const Skills = () => {
 </div>
 <div className="md:w-72 h-82 m-8 rounded-2xl p-8 flex flex-col justify-center items-center bg-newblack">
     <img src={img2} alt="1"></img><br></br>
-    <p>MARKETING & Brand Building </p><br></br>
+    <p className="uppercase">MARKETING & Brand Building </p><br></br>
     <p>With Strong Experience in Marketing & Stratergising have helped over 30+ brands. My deeper interest targeting on Affiliated marketing also creating in-app marketing element also using data analytics for post analytics to have higher engagement with TA. My passion for graphics and brand visualization fuels me to make brand with world class standards.. </p>
 </div>
 </div>
@@ -31,12 +31,12 @@ const Skills = () => {
 
 <div className="">
 <div className="md:w-72 h-82  m-8 rounded-2xl p-8 flex flex-col justify-center items-center bg-newblack">
-    <img src={img1} alt="1"></img><br></br>
+    <img src={img3} alt="1"></img><br></br>
     <p>Product Management</p><br></br>
     <p>Having a great vision to forecast product growth, With strong understanding of business model creation, product architecture, product design, marketing, financial metrics, scalability, and continuous learning. My strong market validation with conversation with  10,000 like Top VCs, angels, HNIs, top executives, and professionals. </p>
 </div>
     <div className="md:w-72 h-82  m-8 rounded-2xl p-8 flex flex-col justify-center items-center bg-newblack">
-    <img src={img1} alt="1"></img><br></br>
+    <img src={img4} alt="1"></img><br></br>
     <p>Product Management</p><br></br>
     <p>Having a great vision to forecast product growth, With strong understanding of business model creation, product architecture, product design, marketing, financial metrics, scalability, and continuous learning. My strong market validation with conversation with  10,000 like Top VCs, angels, HNIs, top executives, and professionals. </p>
 </div>
