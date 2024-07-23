a4ce7ec2d95aeb8229d890d90e58184ca1f1830b
Jaison E Mathew <jaisonemathew14@gmail.com>
2024-07-23 21:27:01 +0530

    added socials link and fixed text

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,6 +1,6 @@
 
 import './Hero.css';
-import { FaLinkedin, FaTwitter, FaInstagram, FaFacebook, FaYoutube, FaEnvelope } from 'react-icons/fa';
+import { FaPinterest ,FaLinkedin, FaTwitter, FaInstagram, FaFacebook, FaYoutube, FaEnvelope } from 'react-icons/fa';
 
 import heroImage from '../../assets/image.webp';
 const Hero = () => {
@@ -12,7 +12,7 @@ const Hero = () => {
       </div>
       <div className="ml-4 mr-4 md:mt-16 text sm:text-left text-center md:max-w-[755px] md:max-h-[658]">
         <h1 className="md:text-5xl text-4xl gradient-h1 poppins-regular ">Joshua Kanatt</h1>
-        <p className='jost-200 text-xl leading-10 text-neutral-400 tracking-[3px]'>
+        <p className='jost-200 text-xl leading-10 text-neutral-400 md:tracking-[3px]'>
         Founder & Chief Executive Officer - GAMERS TAG</p>     
         <p className='poppins-extralight mt-2 gradient-text md:tracking-widest'>
         I'm a visionary entrepreneur with over four years of expertise in Product management, architecture, Marketing, Data analytics, Business development & Consulting, and Startups. Designed over 100 world-class products, setting new industry standards. With unique talent for forecasting product growth, validated by extensive customer persona analysis. As an entrepreneur by heart, I bring a competitive mindset to product ownership,
@@ -20,26 +20,26 @@ assuring highest performance and innovation with mindset of 5x growth. Have a vi
         </p>
         
         <br></br>
-    <p className='md:text-2xl text-lg poppins-extralight  bg-clip-text tracking-[3px]'>"The journey of a thousand miles begins <br></br>with a single innovative step."</p>
+    <p className='md:text-2xl text-lg poppins-extralight  bg-clip-text md:tracking-[3px]'>"The journey of a thousand miles begins <br></br>with a single innovative step."</p>
     <p className='md:mr-20 md:text-right text-center text-xl poppins-extralight'>- Joshua Kanatt</p>
  
     <div className="social-media-buttons md:mt-10">
-    <a href="https://www.linkedin.com" target="google.com" rel="noopener noreferrer">
+    <a href="https://www.instagram.com/joshua_kanatt/" target="google.com" rel="noopener noreferrer">
       <FaLinkedin />
     </a>
-    <a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer">
+    <a href="https://x.com/joshua_kanatt/" target="_blank" rel="noopener noreferrer">
       <FaTwitter />
     </a>
-    <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
+    <a href="https://www.instagram.com/joshua_kanatt/" target="_blank" rel="noopener noreferrer">
       <FaInstagram />
     </a>
-    <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
-      <FaFacebook />
-    </a>
-    <a href="https://www.youtube.com" target="_blank" rel="noopener noreferrer">
+<a href="https://in.pinterest.com/joshuakanatt/" target="_blank" rel="noopener noreferrer">
+  <FaPinterest />
+</a>
+    <a href="https://www.youtube.com/@joshuakanatt" target="_blank" rel="noopener noreferrer">
       <FaYoutube />
     </a>
-    <a href="mailto:your-email@example.com">
+    <a href="mailto:Joshuakanatt66@gmail.com">
       <FaEnvelope />
     </a>
   </div>
